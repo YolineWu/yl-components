2027570217f38dc45253a3a74b3d252540923cc8
YolineWu <wu.yoline@gmail.com>
2023-11-20 20:05:55 +0800

    1、修复 `yl-modal` 组件插槽范围值传递方式错误问题  2、版本号: 0.0.4 --> 0.0.7

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -4,30 +4,34 @@
     :class="['c-yl-modal', { 'c-yl-modal--center': center }]"
     @click="clickMask"
   >
-    <slot v-bind="stateData"></slot>
+    <slot :data="stateData" />
     <div v-if="!$slots['default']" class="c-yl-modal__wrap">
-      <slot name="title" v-bind="{ title }"></slot>
+      <slot name="title" :title="title" />
       <div class="c-yl-modal__title" v-if="!$slots['title'] && title">
         {{ title }}
       </div>
-      <slot name="content"></slot>
+      <slot name="content" :content="content" />
       <div class="c-yl-modal__main" v-if="!$slots['content']">
         {{ content }}
       </div>
-      <slot name="desc"></slot>
+      <slot name="desc" :desc="desc" />
       <div class="c-yl-modal__desc" v-if="!$slots['desc'] && desc">
         {{ desc }}
       </div>
-      <slot name="action-bar"></slot>
+      <slot
+        name="action-bar"
+        :clickCancel="clickCancel"
+        :clickConfirm="clickConfirm"
+      />
       <div class="c-yl-modal__action-bar" v-if="!$slots['action-bar']">
         <div class="c-yl-modal__btn-wrap c-yl-modal__btn-wrap--cancel">
-          <slot name="cancel"></slot>
+          <slot name="cancel" :clickCancel="clickCancel" />
           <button v-if="!$slots['cancel']" @click="clickCancel">
             {{ cancelText }}
           </button>
         </div>
         <div class="c-yl-modal__btn-wrap c-yl-modal__btn-wrap--confirm">
-          <slot name="confirm"></slot>
+          <slot name="confirm" :clickConfirm="clickConfirm" />
           <button v-if="!$slots['confirm']" @click="clickConfirm">
             {{ confirmText }}
           </button>
